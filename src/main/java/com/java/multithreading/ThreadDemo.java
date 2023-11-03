c6c593c5939317f35034f9480461c6ce4fcf101e
SG0305284 <aakash.singh@sabre.com>
2023-11-03 12:25:12 +0530

    Added new problem and notes

--- src/main/java/com/java/multithreading/ThreadDemo.java
+++ src/main/java/com/java/multithreading/ThreadDemo.java
@@ -2,8 +2,9 @@ package com.java.multithreading;
 
 public class ThreadDemo {
 
-    public static void main(String[] args) throws InterruptedException {
+    public  static void main(String[] args) throws InterruptedException {
         // For extending Thread
+
         /*MyThread thread=new MyThread();
         thread.start();
         for (int i = 0; i < 10; i++) {
@@ -11,11 +12,22 @@ public class ThreadDemo {
         }*/
 
         //For implementing Runnable
-        MyThreadUsingRunnable usingRunnable=new MyThreadUsingRunnable();
+
+        /*MyThreadUsingRunnable usingRunnable=new MyThreadUsingRunnable();
         Thread thread=new Thread(usingRunnable);
         thread.start();
         for (int i = 0; i < 100; i++) {
             System.out.println("----Main Thread---");
+        }*/
+
+        //For Dead Lock
+
+        System.out.println("For Dead Lock");
+        Thread.currentThread().join();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("----Main Thread---");
         }
+
     }
 }
--- src/main/java/com/java/practice/PracticeApplication.java
+++ src/main/java/com/java/practice/PracticeApplication.java
@@ -8,6 +8,12 @@ public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+
+		if( "45.1250 52.2500 60.5000".equals(Algorithm.getSharePrices(50,1.1,0.95,2)))
+			System.out.println("Success!");
+		else
+			System.out.println("FAIL!");
+
 	}
 
 }
